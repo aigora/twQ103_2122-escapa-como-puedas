dcab75ff72ebbfbfaff28977d0d1a142d15cdcd8
Moneiba <moneiba.marrero@alumnos.upm.es>
2022-04-08 21:15:12 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+
+typedef struct {
+char nombre_integrantes [20];
+char capitan[20];
+char correo[50];
+} equipo;
+
+struct grupo{
+char nombre[20];
+equipo introduccion[2];
+};
 
 
 int main () {
-
-
+	
+	int i=0,j=0,m=0;
+	char opcion;
+	int edad,numero;
+	struct grupo grupos[2];
 
 
 
@@ -43,13 +59,77 @@ printf("     ¶¶   ¶¶¶           ¶¶¶¶¶¶¶¶¶           ¶¶¶   ¶¶       \n");
 printf("      ¶¶  ¶¶                                   ¶¶  ¶¶      \n");
 printf("       ¶¶¶¶                                     ¶¶¶¶       \n");
 
-printf("\n");
 
-printf("¿Comenzar partida?\n")
-printf("Si\n");
-printf("No");
+printf("Bienvenidos a ESCAPATE COMO PUEDAS\n");
+printf("A continuacion debes elegir una de las dos opciones;\n\n");
+
+do {
+    fflush(stdin);
+    printf("Elige la opcion deseada:\n");
+    printf("A - Estomos preparados para todo.\n");
+    printf("B - Nosotros preferimos irnos.\n");
+    fflush (stdin);
+    scanf("%c", &opcion);
+                
+		switch (opcion) {
+	
+        
+        	case 'A':
+        		printf("Indicanos tu edad para continuar:\n");
+        		scanf("%d", &edad);
+        		if (edad>16) {
+				
+        			for (i=0;i<2;i++) {
+		
+						printf("\n\nIntroduce el nombre del equipo %d:\n",i+1);
+						fflush(stdin);
+						gets(grupos[i].nombre);
+		
+						printf("Ahora vamos con el ultimo paso, conocer a nuestros rivales:\n");
+		
+		
+						for(j=0;j<1;j++) {
+							printf("Indica el numero de integrantes");
+							scanf("%d", &numero);
+						
+						
+							for(m=0;m<numero;m++) {
+								
+								printf ("\nEscribe el nombre del integrantre %d: \n",m+1);
+								fflush(stdin);
+								gets(grupos[i].introduccion[i].nombre_integrantes);
+								fflush(stdin);
+							}
+		
+						printf ("\nEscribe el nombre de vuestro capitan: \n");
+						fflush(stdin);
+						gets(grupos[i].introduccion[i].capitan);
+		
+						printf ("Escribe un correo electronico: \n");
+						gets(grupos[i].introduccion[i].correo);
+			
+					}
+				
+				}
+		} else {
+			printf("Lo sentimos, debes ser mayor de 16.\n");
+				return 0;
+			} 
+				break;
+				
+			case 'B':		
+                	printf("Se procedera a salir del juego.\n");
+                		return 0;
+                	break;
+                	
+                	
+	} 
+
+} while (opcion!='B');
+	printf("\n\nERROR 80028F10. No se ha podido cargar el juego");
+	
+	
+return 0; 
+} 
 
 
-
-
-}
