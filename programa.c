0075caef5de6a76e57ee50a9ebb26f05ac19785b
Moneiba <moneiba.marrero@alumnos.upm.es>
2022-05-22 14:02:06 +0200

    Update programa.c

--- programa.c
+++ programa.c
@@ -60,7 +60,7 @@ return;
 int main () {
 	
 	int i=0,j=0,m=0;
-	int respuesta1,respuesta2;
+	int respuesta1,respuesta2,respuesta3, respuesta4;
 	int solucion2=4;
 	char opcion;
 	int edad,numero;
@@ -147,7 +147,8 @@ do {
 					printf("Os encontrais dentro del laboratorio donde se origino la cura del coronavirus.Debereis resolver los distintos escenarios h¡antes de que se acabe el tiempo\n");
 					printf("Os dirigireis a la primera puerta a la derecha donde encontrareis vuestra primera prueba\n");
 					printf("Mucha suerte. Confiamos en vosotros\n");
-					printf("\n\n\nPRIMERA PREGUNTA\n");
+					
+					printf("\n\n\nPRIMERA PREGUNTA. EQUIPO 1\n");
 					printf("\nBienvenidos al ordenador general. Para esta prueba contareis conun tiempo de 15seg, donde debereis marcar la opcion correcta para abrir la siguiente sala.\n");
 					
 					
@@ -174,34 +175,79 @@ do {
                  		break;
        					}
        		
- 						}while(respuesta1!=3);  
-						printf("RESPUESTA COORECTA\n");
+ 					}while(respuesta1!=3);  
+					printf("RESPUESTA COORECTA\n");
 						
 						
 						
 						
-						system("cls");
+					system("cls");
 	                    
 	                       
-	 					printf("\n\n\nSEGUNDA PREGUNTA\n");
+	 				printf("\n\n\nSEGUNDA PREGUNTA. EQUIPO 1\n");
 	 
 	 
-	 					printf("Varios policías vigilan un cuartel de delincuentes en el que quieren infiltrarse pero para el que necesitan una contraseña.\n");
-						 printf("Una persona llama a la puerta del local y una voz dice desde el interior 8, a lo que esa persona contesta 4. Llega otro y le dicen 14, a lo que contesta 7.\n");
-	 					printf("Uno de los policías llega a la puerta del local, La voz del interior le dice 0, a lo que el responde 0\n");
-	 					printf("Sin embargo, en lugar de dejarle pasar le disparan.\n");
-	 					printf("Averigue cual es la contraseña\n");
+	 				printf("Varios policías vigilan un cuartel de delincuentes en el que quieren infiltrarse pero para el que necesitan una contraseña.\n");
+					printf("Una persona llama a la puerta del local y una voz dice desde el interior 8, a lo que esa persona contesta 4. Llega otro y le dicen 14, a lo que contesta 7.\n");
+	 				printf("Uno de los policías llega a la puerta del local, La voz del interior le dice 0, a lo que el responde 0\n");
+	 				printf("Sin embargo, en lugar de dejarle pasar le disparan.\n");
+	 				printf("Averigue cual es la contraseña\n");
 
 
-	 					do {
-	 						printf("Introduzca la respuesta a 0:\n");
-	 						fflush(stdin);
-	 						scanf("%d", &respuesta2);
+	 				do {
+	 					printf("Introduzca la respuesta a 0:\n");
+	 					fflush(stdin);
+	 					scanf("%d", &respuesta2);
 	 	
-	 						}while(respuesta2!=4);
+	 				}while(respuesta2!=4);
 	 
-						 printf("RESPUESTA CORRECTA. La contraseña no consistía en dividir el número entre dos, sino en decir el número de letras que tenía cada número que les decían.");
-                		 
+					printf("RESPUESTA CORRECTA. La contraseña no consistía en dividir el número entre dos, sino en decir el número de letras que tenía cada número que les decían.");
+						 
+					system("cls"); 
+					
+					printf("\n\n\nPRIMERA PREGUNTA. EQUIPO 2\n");
+					do {
+	
+						printf("Escoja la opcion adecuada sobre el uso del tipo de mascarilla:\n");
+  						printf("Opción 1: Las mascarillas higienicas no son un producto sanitario. Funcionan como un complemento de protección y seguridad.\n");
+						printf("Opcion 2: Las mascarillas quirúrgicas, evitan que cualquier persona pueda alcanzar agentes infecciosos.\n");
+						printf("Opcion 3: Las mascarillas EPI filtran el aire exalado para evitar la salida de organismos como virus.\n");
+						printf("Opcion 4: Las mascarillas higienicas están fabricadas a base de materiales textiles y pero no son reutilizables.\n");
+  						scanf("%d", &respuesta3);
+  						
+  						switch(respuesta1)
+  						{
+                 		case 1 : printf("Opción 1\n");
+                        break;
+                 		case 2 : printf("Opción 2--ERROR\n");
+                        break;
+                 		case 3 : printf("Opción 3--ERROR\n");
+                        break;
+                 		case 4 : printf("Opción 4--ERROR\n");
+                    	break;
+                 		default : printf("Opción desconocida\n"); 
+                 		break;
+       					}
+       		
+ 					}while(respuesta1!=1); 
+					  
+					printf("RESPUESTA COORECTA\n");
+                		
+						
+					system("cls");
+						
+					printf("\n\n\nSEGUNDA PREGUNTA. EQUIPO 2\n");
+					
+					printf("Debeis completar la serie numerica para poder continuar\n");
+					printf(" 5 6 9 6 7 6 ¿?\n");
+					do{
+						printf("¿Que numero falta?\n");
+						scanf("%d", &respuesta4);
+					
+					}while(respuesta3!=7);
+					
+					printf("RESPUESTA CORRECTA\n");
+					 
 							return 0;
 							
 							break;
